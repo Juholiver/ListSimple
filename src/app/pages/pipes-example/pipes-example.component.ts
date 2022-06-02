450dee70ce7b32ddae57a93a9d9582ff69cfb8e7
juholiver <juholiver3@gmail.com>
2022-06-02 17:50:33 -0300

    EncadeandoPipes

--- src/app/pages/pipes-example/pipes-example.component.ts
+++ src/app/pages/pipes-example/pipes-example.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class PipesExampleComponent implements OnInit {
   number = 0;
   text = 'hello world!';
+  date = new Date;
   constructor(private upperCasePipe: UpperCasePipe) { }
 
   ngOnInit(): void {
