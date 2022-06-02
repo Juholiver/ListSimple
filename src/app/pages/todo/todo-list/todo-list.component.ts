95d24b61f627768a4bb18486687bbb536fb79008
juholiver <juholiver3@gmail.com>
2022-06-02 11:04:56 -0300

    Adc Logica no Id TodoList

--- src/app/pages/todo/todo-list/todo-list.component.ts
+++ src/app/pages/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-
+  todos: Array<Todo> = [];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  addTodo(title: string) {
+    const id = this.todos.length + 1
+    this.todos.push(new Todo(id, title, false));
+    console.log(this.todos)
+  }
 }
